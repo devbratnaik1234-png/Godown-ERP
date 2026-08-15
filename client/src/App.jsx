10d664d659aa51de3cf16164c5cc1f8dbb2b4350
sreya-rana <sreyarana123@gmail.com>
2026-08-16 01:21:32 +0530

    Complete truck register module

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,30 +8,81 @@ import Purchase from "./Pages/Purchases/Purchase";
 import Farmers from "./Pages/Farmers/Farmers";
 import Stock from "./Pages/Stock/Stock";
 import Payments from "./Pages/Payments/Payments";
+import TruckRegister from "./Pages/Truck/TruckRegister";
+
 import Labours from "./Pages/Labours/Labours";
 import Reports from "./Pages/Reports/Reports";
 import Settings from "./Pages/Settings/Settings";
+
 function App() {
   return (
     <div className="flex bg-gray-100 min-h-screen">
+
       <Sidebar />
 
       <div className="flex-1">
+
         <Navbar />
 
         <div className="p-8">
+
           <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/purchase" element={<Purchase />} />
-            <Route path="/farmers" element={<Farmers />} />
-            <Route path="/stock" element={<Stock />} />
-            <Route path="/payments" element={<Payments />} />
-            <Route path="/labours" element={<Labours />} />
-            <Route path="/reports" element={<Reports />} />
-            <Route path="/settings" element={<Settings />} />
+
+            <Route
+              path="/"
+              element={<Dashboard />}
+            />
+
+            <Route
+              path="/purchase"
+              element={<Purchase />}
+            />
+
+            <Route
+              path="/farmers"
+              element={<Farmers />}
+            />
+
+            <Route
+              path="/stock"
+              element={<Stock />}
+            />
+
+            {/* Truck Register */}
+            <Route
+              path="/truck"
+              element={<TruckRegister />}
+            />
+
+            <Route
+              path="/payments"
+              element={<Payments />}
+            />
+
+            {/* Labour Management */}
+            <Route
+              path="/labours"
+              element={<Labours />}
+            />
+
+            {/* Reports */}
+            <Route
+              path="/reports"
+              element={<Reports />}
+            />
+
+            {/* Settings */}
+            <Route
+              path="/settings"
+              element={<Settings />}
+            />
+
           </Routes>
+
         </div>
+
       </div>
+
     </div>
   );
 }
