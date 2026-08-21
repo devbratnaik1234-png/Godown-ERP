99f0f94bf1f4a85011fff34be4946ecab97855b5
Samarjit Chatterjee <samarjitchatterjee41@gmail.com>
2026-08-21 23:03:52 +0530

    feat: load dashboard aggregates from backend

--- client/src/Pages/Dashboard/Dashboard.jsx
+++ client/src/Pages/Dashboard/Dashboard.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import DashboardCards from "../../components/Cards/DashboardCards";
 import QuickActions from "../../components/Cards/QuickActions";
 import StockSummary from "../../components/Cards/StockSummary";
@@ -5,11 +6,35 @@ import StockChart from "../../components/Cards/StockChart";
 import RecentPurchases from "../../components/Tables/RecentPurchases";
 import FinancialSummary from "../../components/Cards/FinancialSummary";
 import LiveStockStatus from "../../components/Cards/LiveStockStatus";
+import { api } from "../../api";
 
 function Dashboard() {
+  const [dashboardData, setDashboardData] = useState({});
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const loadDashboard = async () => {
+      try {
+        setError("");
+        const data = await api.dashboard();
+        setDashboardData(data);
+      } catch (err) {
+        setError(err.message);
+      }
+    };
+
+    loadDashboard();
+  }, []);
+
   return (
     <>
-      <DashboardCards />
+      {error && (
+        <div className="mb-4 rounded-lg bg-red-50 p-3 text-red-700">
+          {error}
+        </div>
+      )}
+
+      <DashboardCards data={dashboardData} />
 
       <div className="grid lg:grid-cols-3 gap-6 mt-8">
         <div className="lg:col-span-2">
